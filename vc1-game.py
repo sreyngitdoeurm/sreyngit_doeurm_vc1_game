a54f972a1a997d40f4fd4583b6964203499eddfb
Student <student@PNCLB32-20.pnc.lan>
2021-02-26 09:33:41 +0700

    #3. create walls

--- vc1-game.py
+++ vc1-game.py
@@ -12,11 +12,65 @@ frame.master.title("SREYNGIT GAME")
 # HERE YOU CAN START TO DRAW
 # canvas is like "svg tag" in HTML, it allows user to draw shapes
 canvas = tk.Canvas(frame)
+grid = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1],
+        [1,0,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
+        [1,0,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,1],
+        [1,0,0,0,0,1,1,0,1,1,0,1,1,1,1,1,0,1,0,1],
+        [1,0,1,1,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1],
+        [1,1,1,0,0,0,1,0,1,1,1,1,1,0,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,1,1,0,0,0,1,1,0,1,0,0,0,1],
+        [1,0,1,0,1,1,1,0,1,1,1,0,1,0,0,1,1,1,0,1],
+        [1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,1,0,1,0,1],
+        [1,0,1,1,1,1,1,1,1,0,1,1,1,0,0,1,0,1,0,1],
+        [1,0,1,0,1,0,0,0,0,0,1,0,0,0,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,1,1,1,1,0,1,0,0,1,0,1,0,1],
+        [1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
+        [1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,0,1],
+        [1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+# squareSize = #choose the appropriate size of the squares
+square_size = 30
 
+# VARIABLES
+def arrayToDrawing(array, size):
+    number=0
+    for index in array:
+        number+=1
+        for i in range(len(index)):
+            x0 = i*size
+            y0 = 30*number
+            x1 = x0 + size
+            y1 = y0 + size
+            if index[i] == 1:
+                canvas.create_rectangle(x0, y0, x1, y1, outline="blue", fill = "blue")
+            else:
+                canvas.create_rectangle(x0, y0, x1, y1, fill = "white")
+def moveRight(event):
+    global grid
+    for index in grid:
+        for i in range(len(index)):
+            if index[i] == 1:
+                indexOne = i
+            if indexOne < len(index) - 1:
+                index[indexOne] = 0
+                index[indexOne + 1] = 1
+        arrayToDrawing(grid, square_size)
+
+def moveLeft(event):
+    global grid
+    for i in range(len(grid)):
+        if grid[i] == 1:
+            indexOne = i
+        if indexOne > 0:
+            grid[indexOne] = 0
+            grid[indexOne - 1] = 1
+        arrayToDrawing(grid, square_size) 
 
 # pack means "draw what i put inside"
 canvas.pack(expand=True, fill='both')
 frame.pack(expand=True, fill='both')
-
+arrayToDrawing(grid, square_size)
 # Display all
 root.mainloop()
